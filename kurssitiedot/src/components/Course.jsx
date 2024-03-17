2ea48a7a8e9ee64b52ce2181f5bd74af67489251
olavizz <149702276+olavizz@users.noreply.github.com>
2024-03-18 00:42:09 +0200

    Add files via upload

---
+++ kurssitiedot/src/components/Course.jsx
@@ -0,0 +1,49 @@
+const Course = ({course}) => {
+    return (
+      <div>
+        <Header course={course}/>
+        <Content course={course}/>
+        <Total course={course}/>
+      </div>
+    )
+  }
+  
+  const Header = (props) => {
+    return (
+        <div>
+            <h2>{props.course.name}</h2>
+        </div>
+    )
+  }
+  
+  const Part = (props) => {
+      return (
+        <div>
+            <p>{props.part.name} {props.part.exercises}</p>
+        </div>
+      )
+  }
+  
+  const Content = ({course}) => {
+    return (
+      <div>
+        {course.parts.map(part =>
+        <Part key={part.id} part={part}/>
+        )}
+      </div>
+    )
+  }
+  
+  const Total = ({course}) => {
+    let gg = course.parts.reduce((summa, part)=> summa + part.exercises, 0)
+  
+    return (
+        <div>
+           <h3>total of {gg} exercises</h3>
+        </div>
+  
+    )
+  
+  }
+
+export default Course
